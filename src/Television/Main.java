8eddacbeac86200fad8784db94d48b1084443f3b
Jaden Manuel <jadenmanuel2006@gmail.com>
2021-06-09 09:49:47 -0400

    Getting to go online

---
+++ src/Television/Main.java
@@ -0,0 +1,28 @@
+package Television;
+
+import javax.security.auth.login.LoginException;
+
+
+
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.JDA;
+
+public class Main {
+	public static net.dv8tion.jda.api.JDA jda;
+	
+	
+	public static void main(String[] args) throws LoginException {
+		//jda = (new JDABuilder(AccountType.BOT)).setToken("ODUxODg5Mjk0Nzk1OTk3Mjcz.YL-1uw.1dyGK_Wt_8Xh5aCW4vdYYDBWZ3M").buildAsync();
+		 JDABuilder builder = JDABuilder.createDefault("ODUxODg5Mjk0Nzk1OTk3Mjcz.YL-1uw.1dyGK_Wt_8Xh5aCW4vdYYDBWZ3M");
+		 //builder.setToken("ODUxODg5Mjk0Nzk1OTk3Mjcz.YL-1uw.1dyGK_Wt_8Xh5aCW4vdYYDBWZ3M");
+		 
+		 builder.setStatus(OnlineStatus.ONLINE); 
+		 jda = builder.build();
+		 
+		 
+		
+	}
+	
+}
